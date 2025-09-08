45527cc68511ae7641dbd28599036d85725d6483
Merylp2 <merylputrapratama06@gmail.com>
2025-09-09 06:00:19 +0700

    Update Midtrans

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -34,7 +34,7 @@ class PaymentController extends Controller
 
         // Siapkan parameter Snap
         $params = [
-            'enabled_payments' => ['qris', 'bank_transfer'],
+            'enabled_payments' => ['qris', 'gopay', 'shopeepay', 'bank_transfer'],
             'transaction_details' => [
                 'order_id'     => $transaction->midtrans_order_id,
                 'gross_amount' => (int) $transaction->total,
@@ -180,7 +180,7 @@ public function payHelm(HelmTransaction $helm_transaction): View
     $total = $helm_transaction->helmitems->sum('harga');
 
     $params = [
-        'enabled_payments' => ['qris', 'bank_transfer'],
+        'enabled_payments' => ['qris', 'gopay', 'shopeepay', 'bank_transfer'],
         'transaction_details' => [
             'order_id'     => $helm_transaction->midtrans_order_id,
             'gross_amount' => (int) $total,
