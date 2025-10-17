66c31ccdd1aa0c50035b7e825a287544748c4bd8
Merylp2 <merylputrapratama06@gmail.com>
2025-10-18 04:57:30 +0700

    loaders

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,22 +5,17 @@
     {{-- Required meta tags --}}
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:title" content="Guegbyuur"/>
-    <meta property="og:description" content="Application for Cashier"/>
-    <meta property="og:url" content="https://guegbyuur.up.railway.app"/>
-    <meta property="og:image" content="https://guegbyuur.up.railway.app/images/preview.png"/>
-    <meta property="og:type" content="website"/>
-    <meta name="author" content="Annas Habibi">
-    <meta name="description" content="Aplikasi POS Steam Gue Gbyuur">
+    <meta name="description" content="Aplikasi POS Steam Gue Gbyuur Laravel 11">
     <meta name="author" content="Annas Habibi">
 
     {{-- Title --}}
-    <title>Aplikasi POS Steam Gue Gbyuur Laravel 11</title>
+    <title>Aplikasi POS Steam Gue Gbyuur</title>
 
     {{-- Favicon icon --}}
     {{-- <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon"> --}}
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     {{-- Bootstrap CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     {{-- Tabler Icons CSS --}}
@@ -65,16 +60,8 @@
         </div>
     </main>
 
-    {{-- Footer --}}
-    {{-- <footer class="footer bg-white shadow mt-auto py-3">
-        <div class="container">
-            <div class="d-flex flex-column flex-md-row align-items-center align-items-md-left">
-                <div class="copyright text-center mb-2 mb-md-0">
-                    &copy; 2024 - <a href="https://www.instagram.com/annashabibi_" target="_blank" class="fw-semibold">Gue Gbyuur</a>. All rights reserved.
-                </div>
-            </div>
-        </div>
-    </footer> --}}
+    {{-- Global Loader --}}
+    <x-loader />
 
     {{-- Bootstrap JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -100,15 +87,30 @@
     
 
     <script>
-        document.getElementById('burger').addEventListener('change', function() {
-    const navbar = document.getElementById('navbarMenu');
-    if(this.checked){
-        new bootstrap.Collapse(navbar, { toggle: true });
-    } else {
-        bootstrap.Collapse.getInstance(navbar)?.hide();
-    }
-});
-        // menampilkan pesan dengan sweetalert
+    document.getElementById('burger').addEventListener('change', function() {
+        const navbar = document.getElementById('navbarMenu');
+            if(this.checked){
+            new bootstrap.Collapse(navbar, { toggle: true });
+            } else {
+            bootstrap.Collapse.getInstance(navbar)?.hide();
+        }
+    });
+
+    function showLoader() {
+    document.getElementById('global-loader').classList.remove('hidden');
+  }
+
+  function hideLoader() {
+    document.getElementById('global-loader').classList.add('hidden');
+  }
+
+  // Otomatis muncul saat mulai load
+  document.addEventListener('DOMContentLoaded', showLoader);
+
+  // Hilang setelah halaman selesai dimuat
+  window.addEventListener('load', hideLoader);
+
+        // sweetalert
         @if (session('success'))
             Swal.fire({
                 icon: "success",
